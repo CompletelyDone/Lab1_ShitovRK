40bfb59e8437abe983a19d4d644fc57ba4ea1133
Roman <roman.songoku.army@gmail.com>
2023-09-07 12:12:03 +0700

    Add login validation

---
+++ laba/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DBManager bd = new DBManager();
+            String login;
+            String password;
+            String passwordVerify;
+            Console.WriteLine("Registration");
+            Console.WriteLine("Input login(+x-xxx-xxx-xxxx)");
+            login = Console.ReadLine();
+            if(Validation.ValidateLogin(login))
+            {
+                Console.WriteLine("Login good");
+            }
+            else
+            {
+                Console.WriteLine("Login bad");
+            }
+            
+            Console.WriteLine("Input password");
+            password = Console.ReadLine();
+            Console.WriteLine("Input password");
+            passwordVerify = Console.ReadLine();
+
+            Console.WriteLine("Login: " + login + "\nPassword: " + password + "\n");
+            foreach(Users users in bd.db.Users)
+            {
+                Console.WriteLine(users.userLogin);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ laba/Validation.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba
+{
+    public static class Validation
+    {
+        private static int minLoginLength = 5;
+        public static bool ValidateLogin(String login)
+        {
+            if(login.Count() > minLoginLength)
+            {
+                if (login.StartsWith("+") && login.Count() == 15)
+                {
+                    if (login[2].Equals('-') && login[6].Equals('-') && login[10].Equals('-'))
+                    {
+                        foreach(char c in login)
+                        {
+                            if(!char.IsDigit(c))
+                            {
+                                if(c != '+')
+                                {
+                                    if(c != '-')
+                                    {
+                                        return false;
+                                    }
+                                    return false;
+                                }
+                                return false;
+                            }
+                        }
+                        return true;
+                    }
+                }
+                else if(login.Contains('@') && login.Contains('.'))
+                {
+                    var trimmedEmail = login.Trim();
+                    if (trimmedEmail.EndsWith("."))
+                    {
+                        return false;
+                    }
+                    try
+                    {
+                        var addr = new System.Net.Mail.MailAddress(login);
+                        return addr.Address == trimmedEmail;
+                    }
+                    catch
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    foreach (char c in login)
+                    {
+                        if(!char.IsLetterOrDigit(c))
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool ValidatePassword(String password)
+        {
+            return false;
+        }
+    }
+}
