7c6f82c575887775ccf14ee8ef011aafab8f9378
Roman <roman.songoku.army@gmail.com>
2023-09-23 11:17:56 +0700

    Added test essense

---
+++ TestValidation/UnitTest1.cs
@@ -0,0 +1,21 @@
+namespace TestValidation
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            //Настройка, arrange
+
+            //Действие, act
+
+            //Проверка, assert
+            Assert.Pass();
+        }
+    }
+}
