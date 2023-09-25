03f797d04e474a0239c83767c9fb2f9b0e612e45
Roman <roman.songoku.army@gmail.com>
2023-09-25 10:49:29 +0700

    ноут

--- TestValidation/UnitTest1.cs
+++ TestValidation/UnitTest1.cs
@@ -1,3 +1,5 @@
+using laba;
+
 namespace TestValidation
 {
     public class Tests
@@ -7,15 +9,21 @@ namespace TestValidation
         {
         }
 
-        [Test]
-        public void Test1()
+        [TestCase("Алёша", "password", "password")]
+        [TestCase("Petushara228", "password", "password")]
+        [TestCase("Vasyaloh", "password", "password")]
+        public void Test(string _login, string _password, string _verifyPassword)
         {
             //Настройка, arrange
-
+            var login = _login;
+            var password = _password;
+            var verifyPassword = _verifyPassword;
             //Действие, act
+            var isValidated = Validation.Validate(login, password, verifyPassword);
 
             //Проверка, assert
-            Assert.Pass();
+            Console.WriteLine(isValidated.Item2);
+            Assert.IsFalse(isValidated.Item1);
         }
     }
 }
--- laba/Validation.cs
+++ laba/Validation.cs
@@ -122,7 +122,6 @@ namespace laba
                 returningMessage.Item2 += "Passwords not matched";
             }
 
-
             if(isValidatedPassword && isValidatedLogin && password == verifyPassword)
             {
                 returningMessage.Item1 = true;
@@ -134,59 +133,6 @@ namespace laba
 
 
             return returningMessage;
-        }
-        public static bool ValidateLogin(String login)
-        {
-            if (login.Count() >= minLoginLength)
-            {
-                if (login.StartsWith("+") && login.Count() == 15)
-                {
-                    var regex = new Regex(@"\d{1}-\d{3}-\d{3}-\d{4}");
-                    var isValidated = regex.IsMatch(login);
-                    return isValidated;
-                }
-                else if (login.Contains('@') && login.Contains('.'))
-                {
-                    var trimmedEmail = login.Trim();
-                    if (trimmedEmail.EndsWith("."))
-                    {
-                        return false;
-                    }
-                    try
-                    {
-                        var addr = new System.Net.Mail.MailAddress(login);
-                        return addr.Address == trimmedEmail;
-                    }
-                    catch
-                    {
-                        return false;
-                    }
-                }
-                else
-                {
-                    var latinica = new Regex(@"[A-z]");
-                    var numbers = new Regex(@"[0-9]");
-                    var chara = new Regex(@"[_]");
-                    var isValidated = latinica.IsMatch(login) || numbers.IsMatch(login) || chara.IsMatch(login);
-                    return isValidated;
-                }
-            }
-            return false;
-        }
-        public static bool ValidatePassword(String password)
-        {
-            var noEng = new Regex(@"[^[A-z]");
-            var hasNumber = new Regex(@"[0-9]+");
-            var hasUpperCase = new Regex(@"[А-Я]+");
-            var hasLowerCase = new Regex(@"[а-я]+");
-            var hasSpecSymbol = new Regex(@"\W{1}");
-            var isValidated = noEng.IsMatch(password) 
-                && password.Count() >= minPasswordLength 
-                && hasNumber.IsMatch(password)
-                && hasUpperCase.IsMatch(password)
-                && hasLowerCase.IsMatch(password)
-                && hasSpecSymbol.IsMatch(password);
-            return isValidated;
         }
     }
 }
